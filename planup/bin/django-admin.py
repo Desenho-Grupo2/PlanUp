3b3182a56a89c098f2775c33b9645f9c68ed032e
Victor <guin-hu@hotmail.com>
2018-03-30 16:13:21 -0300

    Update basic project of PlanUp

---
+++ planup/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/victor/Documentos/FGA/Desenho/T1/welcometotheDjango/planup/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
