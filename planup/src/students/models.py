9b8b5d9ace272cedcab2d5f7ddac8baa129fef14
Victor <guin-hu@hotmail.com>
2018-04-03 08:27:47 -0300

    forms templates

--- planup/src/students/models.py
+++ planup/src/students/models.py
@@ -7,6 +7,7 @@ class Student(models.Model):
     email_student = models.EmailField(max_length=40)
     password_student = models.CharField(max_length=14)
     registration_student = models.CharField(max_length=14)
+    age_of_birth_student = models.CharField(max_length=14)
 
     def __unicode__(self):
         return self.name
--- planup/src/students/views.py
+++ planup/src/students/views.py
@@ -1,34 +1,36 @@
-from django.views.generic import TemplateView,ListView
+from django.views.generic import TemplateView, ListView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.core.urlresolvers import reverse_lazy
 from django.views.generic.detail import DetailView
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
 
 from students.models import Student
+from .forms import StudentForm
 
 class StudentList(ListView):
-
     model = Student
 
 class StudentCreate(CreateView):
 
     model = Student
+    form_class = StudentForm
     success_url = reverse_lazy("student_list")
-    fields = ["name_student","email_student","password_student","registration_student"]
+    template_name = "students/student_new.html"
 
 class StudentUpdate(UpdateView):
-
     model = Student
     success_url = reverse_lazy("student_list")
-    fields = ["name_student", "email_student", "password_student","registration_student"]
+    fields = ["name_student", "email_student", "password_student", "registration_student","age_of_birth"]
     template_name_suffix = "_update_form"
 
-class StudentDelete(DeleteView):
 
+class StudentDelete(DeleteView):
     model = Student
     success_url = reverse_lazy("student_list")
 
-class StudentShow(DetailView):
 
+class StudentShow(DetailView):
     model = Student
     success_url = reverse_lazy("student_list")
 
