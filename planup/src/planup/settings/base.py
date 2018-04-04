2da38cce296c553665f3c23ffa981f69636846ab
zero101010 <igotaraujo97@gmail.com>
2018-04-04 14:28:22 -0300

    link the view login with urls with auth

--- planup/src/planup/settings/base.py
+++ planup/src/planup/settings/base.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'students',
     'planup',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -119,7 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-
+OGIN_REDIRECT_URL = 'home'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR,"static"),
 )
--- planup/src/planup/urls.py
+++ planup/src/planup/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import url
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
+from django.views.generic.base import TemplateView
 
 from students import views
 
@@ -10,4 +12,8 @@ urlpatterns = [
     url(r'^editStudent/(?P<pk>\d+)$', views.StudentUpdate.as_view(), name="student_edit"),
     url(r'^deleteStudent/(?P<pk>\d+)$', views.StudentDelete.as_view(), name='student_delete'),
     url(r'^showStudent/(?P<pk>\d+)$', views.StudentShow.as_view(), name="student_show"),
+    #login 
+     url(r'^login/$', auth_views.login, {'template_name': 'login.html'}, name='login'),
+
+
 ]
