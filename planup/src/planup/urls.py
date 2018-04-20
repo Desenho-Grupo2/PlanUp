9cd993d2047ec2788c77efa5c4c3c32ac69e9aa2
zero101010 <igotaraujo97@gmail.com>
2018-04-20 18:25:06 -0300

    "add the method to update the subjects"
    Signed-off-by: zero101010 <igotaraujo97@gmail.com>
    Signed-off-by: FilipeKN4 <filipekn4@gmail.com>

--- planup/src/planup/urls.py
+++ planup/src/planup/urls.py
@@ -5,16 +5,18 @@ from students import views
 import subjects.views
 from django.urls import path
 
-from subjects.views import list_subject, create_subject
+from subjects.views import list_subject, create_subject, update_subject
 
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', views.student_list, name="student_list"),## home page
+    url(r'^$', views.student_list, name="student_list"),  # home page
     url(r'^newStudent$', views.create_student, name="student_new"),
     url(r'^editStudent/(?P<pk>\d+)/$', views.edit_student, name="student_edit"),
-    url(r'^deleteStudent/(?P<pk>\d+)$', views.delete_student, name='student_delete'),
+    url(r'^deleteStudent/(?P<pk>\d+)$',
+        views.delete_student, name='student_delete'),
     url(r'^showStudent/(?P<pk>\d+)$', views.show_student, name="student_show"),
-    path('newSubject/',create_subject, name="new_subject"),
-    path('listSubject',list_subject,name='list_subject'),
+    path('newSubject/', create_subject, name="new_subject"),
+    path('listSubject/', list_subject, name='list_subject'),
+    path('updateSubject/<int:id>', update_subject, name="update_subject"),
 ]
--- planup/src/subjects/views.py
+++ planup/src/subjects/views.py
@@ -4,17 +4,18 @@ from django.shortcuts import render, get_object_or_404, redirect
 
 from subjects.models import Subject
 from .forms import SubjectForm
+import subjects
 
 
 def create_subject(request):
 
-    subject = SubjectForm(request.POST or None)
+    subject_form = SubjectForm(request.POST or None)
 
-    if subject.is_valid():
-        subject.save()
+    if subject_form.is_valid():
+        subject_form.save()
         return redirect('list_subject')
 
-    return render(request, 'subjects/new_subject.html', {'subject': subject})
+    return render(request, 'subjects/new_subject.html', {'subject_form': subject_form})
 
 
 def list_subject(request):
@@ -22,3 +23,14 @@ def list_subject(request):
     subjects = Subject.objects.all()
 
     return render(request, 'subjects/list_subjects.html', {'subjects': subjects})
+
+
+def update_subject(request, id):
+    subject = Subject.objects.get(id=id)
+    subject_form = SubjectForm(request.POST or None, instance=subject)
+
+    if subject_form.is_valid():
+        subject_form.save()
+        return redirect('list_subject')
+
+    return render(request, 'subjects/new_subject.html', {'subject_form': subject_form, 'subject': subject})
